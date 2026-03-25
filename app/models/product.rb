ad52bb7240224c1ff6ea14623f753754f25a2c30
Silviu Pop <silviu.pop25@gmail.com>
2026-03-25 12:36:01 +0200

    new new new

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,16 +3,4 @@ class Product < ApplicationRecord
 
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
-
-
-  def as_json(options = {})
-    super(options.merge(
-      methods: [:brand_name],
-      except: [:brand_id, :created_at, :updated_at]
-    ))
-  end
-
-  def brand_name
-    brand.name
-  end
 end
